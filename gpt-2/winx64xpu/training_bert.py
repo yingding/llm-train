36a8c39a1b3a762fa4864aca65e2a4bbe27f9d57
Yingding Wang <yingdingwang@microsoft.com>
2025-01-12 23:05:36 +0100

    updates

--- gpt-2/winx64xpu/training_bert.py
+++ gpt-2/winx64xpu/training_bert.py
@@ -2,12 +2,14 @@ import torch
 from transformers import BertModel
 
 model = BertModel.from_pretrained("bert-base-uncased")
+model = model.to("xpu")
 model.eval()
 
 vocab_size = model.config.vocab_size
 batch_size = 128
 seq_length = 512
 data = torch.randint(vocab_size, size=[batch_size, seq_length])
+data = data.to("xpu")
 
 #################### code changes ####################  # noqa F401
 import intel_extension_for_pytorch as ipex
