db223c549bb3d5a963e1e7aaeb47572ad9ded115
Yingding Wang <yingdingwang@microsoft.com>
2025-01-17 21:56:37 +0100

    updates

--- gpt-2/winx64npu/ENV.md
+++ gpt-2/winx64npu/ENV.md
@@ -1,26 +1,13 @@
-## Env
-
-use the windows native agent3.11 venv.
-The NPU works only on native windows venv with the 
-```
-intel-npu-acceleration-library==1.4.0
-```
-library
-
 ## NPU environment
+Native window python3.12 venv: `gpt3.12`
 
-Native window python3.12 venv:
-```text
-## the linux package is only for ubuntu
-gpt3.12wsl
-## does the windows package actually work?
-gpt3.12
-gpt3.12conda
+The NPU python lib works at the time of writing (17/01/2025) only with the native windows venv with the 
+```shell
+python -m pip install intel-npu-acceleration-library==1.4.0
 ```
 
-## Examples
-
-* https://github.com/intel/intel-npu-acceleration-library/issues/108#issuecomment-2463827536
+## Issues
+* NPU example workaround https://github.com/intel/intel-npu-acceleration-library/issues/108#issuecomment-2463827536
 
 
 
---
+++ gpt-2/winx64npu/playground/matmul.py
@@ -0,0 +1,47 @@
+from intel_npu_acceleration_library.backend import MatMul
+import numpy as np
+from applyllm.utils import time_func
+from dataclasses import dataclass
+
+
+# @time_func
+# def run_npu_matmul(inC, outC, batch):
+
+#     # Create both inputs
+#     X1 = np.random.uniform(-1, 1, (batch, inC)).astype(np.float16)
+#     X2 = np.random.uniform(-1, 1, (outC, inC)).astype(np.float16)
+
+#     mm = MatMul(inC, outC, batch, profile=False)
+
+#     return mm.run(X1, X2)
+
+@dataclass
+class MatMulConfig:
+    inC: int
+    outC: int
+    batch: int
+
+    def __post_init__(self):
+        # Create both inputs
+        self.X1 = np.random.uniform(-1, 1, (self.batch, self.inC)).astype(np.float16)
+        self.X2 = np.random.uniform(-1, 1, (self.outC, self.inC)).astype(np.float16)
+
+    @time_func
+    def run_npu_matmul(self):
+        mm = MatMul(self.inC, self.outC, self.batch, profile=False)
+        return mm.run(self.X1, self.X2)
+
+    @time_func
+    def run_cpu_matmul(self):
+        np_result = np.matmul(self.X1, self.X2.T)
+        return np_result
+
+
+if __name__ == "__main__":
+    # create a config object with 128, 128, 32
+    config = MatMulConfig(128, 128, 32)
+    # run the npu matmul
+    print(config.run_npu_matmul())
+    print("="*10)
+    # run the cpu matmul
+    print(config.run_cpu_matmul())
